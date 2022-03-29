216de445f3121d67fd29ca5fc45c8f0309291d5f
Amit Singh Rana <ranaa73000@gmail.com>
2022-03-29 12:06:32 +0530

    Added .env, body-parser & cors conifgs

--- server/index.js
+++ server/index.js
@@ -2,8 +2,19 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
 const app = express();
+
+// .env Config
+dotenv.config();
+
+// Body-Parser Setup
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// Cross-Origin request handling
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to server");
