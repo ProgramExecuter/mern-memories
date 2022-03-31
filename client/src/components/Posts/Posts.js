b6090cad5bff86254979936fd688a29710895c29
Amit Singh Rana <ranaa73000@gmail.com>
2022-03-31 11:41:58 +0530

    Connected styles to components

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,11 @@
 import React from "react";
 
+import useStyles from "./styles";
 import Post from "./Post/Post.js";
+
 const Posts = () => {
+  const classes = useStyles();
+
   return (
     <>
       <h1>Posts</h1>
