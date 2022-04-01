631e4dfc1782409f54babd1f8f1238249b63359d
Amit Singh Rana <ranaa73000@gmail.com>
2022-04-01 10:45:05 +0530

    Implementing actions + reducers for fetching posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import useStyles from "./styles";
 import Post from "./Post/Post.js";
 
 const Posts = () => {
   const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
 
+  console.log(posts);
   return (
     <>
       <h1>Posts</h1>
