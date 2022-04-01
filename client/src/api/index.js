7128290272d63b396a163844022ac6034ef2eb79
Amit Singh Rana <ranaa73000@gmail.com>
2022-04-01 13:10:11 +0530

    Implementing actions + reducers for creating posts

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,4 +4,6 @@ const url = "http://localhost:4000/posts";
 
 const fetchPosts = () => axios.get(url);
 
-export { fetchPosts };
+const createPost = (newPost) => axios.post(url, newPost);
+
+export { fetchPosts, createPost };
